c9e5e3cde3d794c5e4ab8d7357ec09901fd1a677
yuldashboev <team19devs@gmail.com>
2022-12-29 18:44:50 +0500

    added admin auth middleware & add user to project when new created

---
+++ api/handlers/middleware_admin.go
@@ -0,0 +1,79 @@
+package handlers
+
+import (
+	"strings"
+	"ucode/ucode_go_api_gateway/api/http"
+	"ucode/ucode_go_api_gateway/genproto/auth_service"
+	"ucode/ucode_go_api_gateway/pkg/helper"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (h *Handler) AdminAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		res, ok := h.hasAccess(c)
+		if !ok {
+			c.Abort()
+			return
+		}
+
+		c.Set("Auth", res)
+		c.Set("namespace", h.cfg.UcodeNamespace)
+		c.Next()
+	}
+}
+
+func (h *Handler) adminHasAccess(c *gin.Context) (*auth_service.HasAccessSuperAdminRes, bool) {
+	bearerToken := c.GetHeader("Authorization")
+	strArr := strings.Split(bearerToken, " ")
+	if len(strArr) != 2 || strArr[0] != "Bearer" {
+		h.handleResponse(c, http.Forbidden, "token error: wrong format")
+		return nil, false
+	}
+	accessToken := strArr[1]
+	resp, err := h.authService.SessionService().HasAccessSuperAdmin(
+		c.Request.Context(),
+		&auth_service.HasAccessSuperAdminReq{
+			AccessToken: accessToken,
+			// ClientPlatformId: "3f6320a6-b6ed-4f5f-ad90-14a154c95ed3",
+			Path:   helper.GetURLWithTableSlug(c),
+			Method: c.Request.Method,
+		},
+	)
+	if err != nil {
+		errr := status.Error(codes.PermissionDenied, "Permission denied")
+		if errr.Error() == err.Error() {
+			h.handleResponse(c, http.BadRequest, err.Error())
+			return nil, false
+		}
+		errr = status.Error(codes.InvalidArgument, "User has been expired")
+		if errr.Error() == err.Error() {
+			h.handleResponse(c, http.Forbidden, err.Error())
+			return nil, false
+		}
+		h.handleResponse(c, http.Unauthorized, err.Error())
+		return nil, false
+	}
+
+	return resp, true
+}
+
+func (h *Handler) adminAuthInfo(c *gin.Context) (result *auth_service.V2HasAccessUserRes, err error) {
+	data, ok := c.Get("Auth")
+
+	if !ok {
+		h.handleResponse(c, http.Forbidden, "token error: wrong format")
+		c.Abort()
+		return nil, err
+	}
+	accessResponse, ok := data.(*auth_service.V2HasAccessUserRes)
+	if !ok {
+		h.handleResponse(c, http.Forbidden, "token error: wrong format")
+		c.Abort()
+		return nil, err
+	}
+
+	return accessResponse, nil
+}
