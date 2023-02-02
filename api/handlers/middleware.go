69e52763ae4bdeacc3bd947a7679c9de3839fc30
yuldashboev <team19devs@gmail.com>
2023-02-02 18:24:22 +0500

    added default case to auth middleware to detect invalid auth methods

--- api/handlers/middleware.go
+++ api/handlers/middleware.go
@@ -107,7 +107,11 @@ func (h *Handler) AuthMiddleware(cfg config.Config) gin.HandlerFunc {
 			}
 			c.Set("resource_id", resource.GetResource().GetId())
 			c.Set("environment_id", apikeys.GetEnvironmentId())
-
+		default:
+			err := errors.New("error invalid authorization method")
+			h.log.Error("--AuthMiddleware--", logger.Error(err))
+			h.handleResponse(c, status_http.BadRequest, err.Error())
+			c.Abort()
 		}
 
 		c.Set("Auth", res)
