177b192ea4ee6745e8ace15a374a885435918658
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-02-20 10:56:14 +0500

    changes

--- pkg/code_server/code_server.go
+++ pkg/code_server/code_server.go
@@ -36,7 +36,7 @@ func CreateCodeServer(functionName string, cfg config.Config, id string) (string
 	}
 	fmt.Println("test helm install code server")
 	var out bytes.Buffer
-	cmd = exec.Command("echo", "$(kubectl", "get", "secret", " --namespace", "test", functionName, "-o", "jsonpath=\"{.data.password}\"", "|", "base64", "-d)")
+	cmd = exec.Command("kubectl", "get", "secret", " --namespace", "test", functionName, "-o", "jsonpath=\"{.data.password}\"", "|", "base64", "-d")
 	if err != nil {
 		return "", errors.New("error while get password 0::" + err.Error())
 	}
