4f479e424b0f951ef5c50ea599e47f95927e2a75
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-02-21 17:57:54 +0500

    test clone

--- pkg/gitlab_integration/git_clone.go
+++ pkg/gitlab_integration/git_clone.go
@@ -1,6 +1,7 @@
 package gitlab_integration
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"os/exec"
@@ -13,13 +14,15 @@ func CloneForkToPath(path string, cfg config.Config) error {
 	// path = strings.TrimPrefix(path, "https://")
 	// command := fmt.Sprintf("https://oauth:%s@%s", cfg.GitlabIntegrationToken, path)
 	fmt.Println("ssh url::", path)
-	cmd := exec.Command("git", "-c", "core.sshCommand=\"ssh -i /key/ssh-privatekey\"", "clone", path)
+	cmd := exec.Command("git", "-c", "core.sshCommand=ssh -i /key/ssh-privatekey", "clone", path)
 	fmt.Println("path clone:::", cfg.PathToClone)
 	cmd.Dir = cfg.PathToClone
 	fmt.Println("test clone")
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
 	err := cmd.Run()
 	if err != nil {
-		return errors.New("could not clone repo into given path::" + cmd.Start().Error())
+		return errors.New("could not clone repo into given path::" + stderr.String())
 	}
 	fmt.Println("test clone 22")
 	fmt.Println("Output::")
