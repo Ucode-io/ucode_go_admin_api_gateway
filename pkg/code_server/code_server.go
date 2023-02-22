d296c14098cb84bf563019775f999ddcd24adeae
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-02-22 10:22:59 +0500

    check install code server

--- pkg/code_server/code_server.go
+++ pkg/code_server/code_server.go
@@ -17,16 +17,21 @@ func CreateCodeServer(functionName string, cfg config.Config, id string) (string
 
 	// command := fmt.Sprintf("--username udevs --password %s code-server https://gitlab.udevs.io/api/v4/projects/1512/packages/helm/stable", cfg.GitlabIntegrationToken)
 	cmd := exec.Command("helm", "repo", "add", "--username", "udevs", "--password", cfg.GitlabIntegrationToken, "code-server", "https://gitlab.udevs.io/api/v4/projects/1512/packages/helm/stable")
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
 	err := cmd.Run()
 	if err != nil {
-		return "", errors.New("error while adding repo:" + err.Error())
+		fmt.Println("err 0::", err)
+		return "", errors.New("error while adding repo:" + stderr.String())
 	}
 	fmt.Println("test repo add")
 
 	cmd = exec.Command("helm", "repo", "update")
 	err = cmd.Run()
 	if err != nil {
-		return "", errors.New("error while repo update helm::" + err.Error())
+		fmt.Println("err 1::", err)
+		return "", errors.New("error while repo update helm::" + stderr.String())
 	}
 
 	hostName := fmt.Sprintf("--set=ingress.hosts[0].host=%s.u-code.io", id)
@@ -37,28 +42,32 @@ func CreateCodeServer(functionName string, cfg config.Config, id string) (string
 	cmd = exec.Command("helm", "install", functionName, "code-server/code-server", "-n", "test", hostName, hostNameTls, secretName, path)
 	err = cmd.Run()
 	if err != nil {
-		return "", errors.New("error while install code server::" + err.Error())
+		fmt.Println("err 2::", err)
+		return "", errors.New("error while install code server::" + stderr.String())
 	}
 	fmt.Println("test helm install code server")
 	var out bytes.Buffer
 	cmd = exec.Command("kubectl", "get", "secret", "--namespace", "test", functionName+"-code-server", "-o", "jsonpath=\"{.data.password}\"")
 	if err != nil {
-		return "", errors.New("error while get password 0::" + err.Error())
+		fmt.Println("err 3::", err)
+		return "", errors.New("error while get password 0::" + stderr.String())
 	}
-	var stderr bytes.Buffer
+
 	cmd.Stdout = &out
 	fmt.Println("aa::", cmd.Stderr)
-	cmd.Stderr = &stderr
+
 	err = cmd.Run()
 	fmt.Println("ss::", cmd.Stdout)
 	fmt.Println("ff::", out.String())
 	if err != nil {
+		fmt.Println("err 4::", err)
 		return "", errors.New("error running get password command::" + stderr.String())
 	}
 
 	s := strings.ReplaceAll(out.String(), `"`, "")
 	str, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
+		fmt.Println("err 5::", err)
 		return "", errors.New("error while base64 to string::" + stderr.String())
 	}
 	pass := fmt.Sprintf("%s", str)
