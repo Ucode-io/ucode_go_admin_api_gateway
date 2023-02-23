23c5dd7cb02fdf5e4c6c10d99992ea1652b3aaf2
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-02-23 18:52:50 +0500

    test delete function

--- pkg/gitlab_integration/git_clone.go
+++ pkg/gitlab_integration/git_clone.go
@@ -32,3 +32,28 @@ func CloneForkToPath(path string, cfg config.Config) error {
 
 	return nil
 }
+
+func DeletedClonedRepoByPath(path string, cfg config.Config) error {
+
+	//git -c core.sshCommand="ssh -i /key/ssh-privatekey” clone git@gitlab.udevs.io:ucode/ucode_go_admin_api_gateway.git
+	// path = strings.TrimPrefix(path, "https://")
+	// command := fmt.Sprintf("https://oauth:%s@%s", cfg.GitlabIntegrationToken, path)
+	fmt.Println("ssh url::", path)
+	// cmd := exec.Command("git", "-c", "core.sshCommand=\"ssh -i /key/ssh-privatekey\"", "clone", path)
+	// sshCommand := "GIT_SSH_COMMAND='ssh -i /key/ssh-privatekey -o IdentitiesOnly=yes'"
+	cmd := exec.Command("rm", "-rf", path) //path ssh url
+	fmt.Println("deleted cloned repo:::", cfg.PathToClone)
+	cmd.Dir = cfg.PathToClone
+	fmt.Println("delete cloned repo")
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("err:::", err)
+		return errors.New("could not delete cloned repo into given path::" + stderr.String())
+	}
+	fmt.Println("test delete clone 22")
+	fmt.Println("Output::")
+
+	return nil
+}
