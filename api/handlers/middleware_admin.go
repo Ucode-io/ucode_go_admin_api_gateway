695bad146d74f978b716c6b0ce46470db9f2879b
valiyev_muhammadjon <valiyevmuhammadjon@gmail.com>
2023-05-25 19:41:15 +0500

    added project id in api key

--- api/handlers/middleware_admin.go
+++ api/handlers/middleware_admin.go
@@ -69,6 +69,7 @@ func (h *Handler) AdminAuthMiddleware() gin.HandlerFunc {
 			}
 
 			c.Set("environment_id", apikeys.GetEnvironmentId())
+			c.Set("project_id", apikeys.GetProjectId())
 		default:
 			err := errors.New("error invalid authorization method")
 			h.log.Error("--AuthMiddleware--", logger.Error(err))
