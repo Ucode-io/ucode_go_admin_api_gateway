e44042d84c0bc11fb60a21d9516ba72b8ea107d0
Islombek Oripov <mymaxs441@gmail.com>
2023-05-29 16:29:39 +0500

    chat service crete bot updated

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -3451,11 +3451,6 @@ const docTemplate = `{
                     },
                     {
                         "type": "string",
-                        "name": "role_id",
-                        "in": "query"
-                    },
-                    {
-                        "type": "string",
                         "name": "search",
                         "in": "query"
                     }
@@ -33072,9 +33067,6 @@ const docTemplate = `{
                 "name": {
                     "type": "string"
                 },
-                "permission": {
-                    "$ref": "#/definitions/object_builder_service.App_Permission"
-                },
                 "tables": {
                     "type": "array",
                     "items": {
@@ -33109,23 +33101,6 @@ const docTemplate = `{
                     "items": {
                         "type": "string"
                     }
-                }
-            }
-        },
-        "object_builder_service.App_Permission": {
-            "type": "object",
-            "properties": {
-                "create": {
-                    "type": "boolean"
-                },
-                "delete": {
-                    "type": "boolean"
-                },
-                "read": {
-                    "type": "boolean"
-                },
-                "update": {
-                    "type": "boolean"
                 }
             }
         },
@@ -34371,32 +34346,6 @@ const docTemplate = `{
                 }
             }
         },
-        "object_builder_service.Permission": {
-            "type": "object",
-            "properties": {
-                "delete": {
-                    "type": "string"
-                },
-                "guid": {
-                    "type": "string"
-                },
-                "is_have_condition": {
-                    "type": "boolean"
-                },
-                "is_public": {
-                    "type": "boolean"
-                },
-                "read": {
-                    "type": "string"
-                },
-                "update": {
-                    "type": "string"
-                },
-                "write": {
-                    "type": "string"
-                }
-            }
-        },
         "object_builder_service.QuickFilter": {
             "type": "object",
             "properties": {
@@ -34720,9 +34669,6 @@ const docTemplate = `{
                 },
                 "project_id": {
                     "type": "string"
-                },
-                "record_permissions": {
-                    "$ref": "#/definitions/object_builder_service.Permission"
                 },
                 "show_in_menu": {
                     "type": "boolean"
