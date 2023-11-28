72c78ebdd2012a998961eff7d2d7f801d1a98504
abdinazarovDV <abdinazarovgithub17@gmail.com>
2023-11-28 13:23:45 +0500

    add test logs

--- api/handlers/menu.go
+++ api/handlers/menu.go
@@ -3,9 +3,11 @@ package handlers
 import (
 	"context"
 	"errors"
+	"fmt"
 	"reflect"
 	"ucode/ucode_go_api_gateway/api/models"
 	"ucode/ucode_go_api_gateway/api/status_http"
+	"ucode/ucode_go_api_gateway/config"
 	pb "ucode/ucode_go_api_gateway/genproto/company_service"
 	obs "ucode/ucode_go_api_gateway/genproto/object_builder_service"
 	tmp "ucode/ucode_go_api_gateway/genproto/template_service"
@@ -283,6 +285,10 @@ func (h *Handler) GetAllMenus(c *gin.Context) {
 	}
 	authInfo, _ := h.GetAuthInfo(c)
 	limit = 100
+
+	if resource.NodeType == config.ENTER_PRICE_TYPE {
+		fmt.Println("\n\n enter price project id ", projectId.(string))
+	}
 
 	services, err := h.GetProjectSrvc(
 		c.Request.Context(),
