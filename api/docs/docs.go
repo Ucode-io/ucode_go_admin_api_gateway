3f85c6095c91ecad25c6668d46ddccd1a2bc0051
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2023-12-05 11:53:19 +0500

    swagger updated

--- api/docs/docs.go
+++ api/docs/docs.go
@@ -46591,8 +46591,6 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
-	LeftDelim:        "{{",
-	RightDelim:       "}}",
 }
 
 func init() {
