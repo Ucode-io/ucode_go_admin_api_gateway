6f4c9e1e399d60fd7bad8be7696aef77f052934c
islombekrakhmonov <islombek.rakhmanov00@gmail.com>
2023-12-23 17:10:27 +0500

    ping to comapny and auth services

--- api/handlers/v1/ping.go
+++ api/handlers/v1/ping.go
@@ -1,13 +1,38 @@
 package v1
 
 import (
+	"context"
 	"fmt"
 	"ucode/ucode_go_api_gateway/config"
+	"ucode/ucode_go_api_gateway/genproto/auth_service"
+	"ucode/ucode_go_api_gateway/genproto/company_service"
 
 	"ucode/ucode_go_api_gateway/api/status_http"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/gin-gonic/gin"
 )
+
+var (
+	pingSuccess = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "ping_success",
+			Help: "Number of successful pings",
+		},
+		[]string{"service"},
+	)
+	pingFail = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "ping_fail",
+			Help: "Number of failed pings",
+		},
+		[]string{"service"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(pingSuccess, pingFail)
+}
 
 // Ping godoc
 // @ID ping
@@ -20,6 +45,40 @@ import (
 // @Failure 500 {object} status_http.Response{}
 func (h *HandlerV1) Ping(c *gin.Context) {
 	fmt.Println("config.CountReq: ", config.CountReq)
+
+	limit, err := h.getLimitParam(c)
+	if err != nil {
+		h.handleResponse(c, status_http.InvalidArgument, err.Error())
+		return
+	}
+
+	offset, err := h.getOffsetParam(c)
+	if err != nil {
+		h.handleResponse(c, status_http.InvalidArgument, err.Error())
+		return
+	}
+	_, err = h.companyServices.Company().GetListWithProjects(
+		context.Background(),
+		&company_service.GetListWithProjectsRequest{
+			Limit:  int32(limit),
+			Offset: int32(offset),
+		},
+	)
+	if err != nil {
+		pingFail.WithLabelValues("company_service").Inc()
+	}
+	pingSuccess.WithLabelValues("company_service").Inc()
+
+
+	_, err = h.authService.User().GetUserProjects(context.Background(), &auth_service.UserPrimaryKey{
+		Id: "",
+	})
+	if err != nil {
+		pingFail.WithLabelValues("auth_service").Inc()
+	}
+
+	pingSuccess.WithLabelValues("auth_service").Inc()
+
 	h.handleResponse(c, status_http.OK, "pong")
 }
 
