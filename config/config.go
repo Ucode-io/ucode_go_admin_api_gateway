2051538908c08761dfafa3cf447474095b1b9fa6
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-02-19 15:55:18 +0500

    new function token

--- config/config.go
+++ config/config.go
@@ -191,7 +191,7 @@ func BaseLoad() BaseConfig {
 	config.DefaultOffset = cast.ToString(GetOrReturnDefaultValue("DEFAULT_OFFSET", "0"))
 	config.DefaultLimit = "100"
 
-	config.GitlabIntegrationToken = "glpat-YueTgsgKyqr_btoWFG2w"
+	config.GitlabIntegrationToken = "glpat-XXKT7Jq88GujqWHqkDgs"
 	config.GitlabIntegrationURL = cast.ToString(GetOrReturnDefaultValue("GITLAB_URL", "https://gitlab.udevs.io"))
 	config.GitlabGroupId = cast.ToInt(GetOrReturnDefaultValue("GITLAB_GROUP_ID", 0))
 	config.GitlabProjectId = cast.ToInt(GetOrReturnDefaultValue("GITLAB_PROJECT_ID", 0))
