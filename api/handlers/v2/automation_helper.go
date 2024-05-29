7953ba3451a68828971db4dc48c95083ea5bc34c
b3atez <azamatn676@gmail.com>
2024-05-29 07:39:00 +0500

    fixed

--- api/handlers/v2/automation_helper.go
+++ api/handlers/v2/automation_helper.go
@@ -192,6 +192,10 @@ func DoInvokeFuntion(request DoInvokeFuntionStruct, c *gin.Context, h *HandlerV2
 		invokeFunction.Data = data
 
 		if customEvent.GetFunctions()[0].RequestType == "" || customEvent.GetFunctions()[0].RequestType == "ASYNC" {
+
+			fmt.Println(request.TableSlug)
+			fmt.Println("IT'S ASYNC FUNCTION")
+
 			resp, err := util.DoRequest("https://ofs.u-code.io/function/"+customEvent.GetFunctions()[0].Path, "POST", invokeFunction)
 			if err != nil {
 				return customEvent.GetFunctions()[0].Name, err
@@ -203,6 +207,9 @@ func DoInvokeFuntion(request DoInvokeFuntionStruct, c *gin.Context, h *HandlerV2
 				return customEvent.GetFunctions()[0].Name, errors.New(errStr)
 			}
 		} else if customEvent.GetFunctions()[0].RequestType == "SYNC" {
+
+			fmt.Println(request.TableSlug)
+			fmt.Println("IT'S SYNC FUNCTION")
 
 			go func() {
 				resp, err := util.DoRequest("https://ofs.u-code.io/function/"+customEvent.GetFunctions()[0].Path, "POST", invokeFunction)
