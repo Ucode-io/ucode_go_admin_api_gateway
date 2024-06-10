9ce149770ee3b4d3635f0b37fbc42d2ce29c7b20
b3atez <azamatn676@gmail.com>
2024-06-10 15:17:46 +0500

    fix get list version

--- api/handlers/v2/version_history.go
+++ api/handlers/v2/version_history.go
@@ -7,6 +7,7 @@ import (
 	"time"
 	"ucode/ucode_go_api_gateway/api/status_http"
 	pb "ucode/ucode_go_api_gateway/genproto/company_service"
+	nb "ucode/ucode_go_api_gateway/genproto/new_object_builder_service"
 	obs "ucode/ucode_go_api_gateway/genproto/object_builder_service"
 	"ucode/ucode_go_api_gateway/pkg/util"
 
@@ -105,7 +106,6 @@ func (h *HandlerV2) GetVersionHistoryByID(c *gin.Context) {
 func (h *HandlerV2) GetAllVersionHistory(c *gin.Context) {
 
 	var (
-		resp             *obs.ListVersionHistory
 		fromDate, toDate string
 		orderby          bool
 	)
@@ -190,7 +190,7 @@ func (h *HandlerV2) GetAllVersionHistory(c *gin.Context) {
 
 	switch resource.ResourceType {
 	case pb.ResourceType_MONGODB:
-		resp, err = services.GetBuilderServiceByType(resource.NodeType).VersionHistory().GatAll(
+		resp, err := services.GetBuilderServiceByType(resource.NodeType).VersionHistory().GatAll(
 			context.Background(),
 			&obs.GetAllRquest{
 				Type:      tip,
@@ -208,23 +208,32 @@ func (h *HandlerV2) GetAllVersionHistory(c *gin.Context) {
 			h.handleResponse(c, status_http.GRPCError, err.Error())
 			return
 		}
-	case pb.ResourceType_POSTGRESQL:
-		// resp, err = services.PostgresBuilderService().View().GetList(
-		// 	context.Background(),
-		// 	&obs.GetAllViewsRequest{
-		// 		TableSlug: c.Param("collection"),
-		// 		ProjectId: resource.ResourceEnvironmentId,
-		// 		RoleId:    roleId,
-		// 	},
-		// )
 
-		// if err != nil {
-		// 	h.handleResponse(c, status_http.GRPCError, err.Error())
-		// 	return
-		// }
+		h.handleResponse(c, status_http.OK, resp)
+	case pb.ResourceType_POSTGRESQL:
+		resp, err := services.GoObjectBuilderService().VersionHistory().GatAll(
+			context.Background(),
+			&nb.GetAllRquest{
+				Type:      tip,
+				ProjectId: resource.ResourceEnvironmentId,
+				EnvId:     currEnvironmentId.(string),
+				ApiKey:    apiKey,
+				Offset:    int32(offset),
+				Limit:     int32(limit),
+				FromDate:  fromDate,
+				ToDate:    toDate,
+				OrderBy:   orderby,
+			},
+		)
+
+		if err != nil {
+			h.handleResponse(c, status_http.GRPCError, err.Error())
+			return
+		}
+
+		h.handleResponse(c, status_http.OK, resp)
 	}
 
-	h.handleResponse(c, status_http.OK, resp)
 }
 
 // UpdateListVersionHistory godoc
