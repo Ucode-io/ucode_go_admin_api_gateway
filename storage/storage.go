a30e570799fdfbe8a3e29b49ceb07a59e54c6a43
Shokhrukh Safarov <safarovshohruh2803@gmail.com>
2024-07-31 09:55:27 +0500

    restart

--- storage/storage.go
+++ storage/storage.go
@@ -10,7 +10,7 @@ import (
 var ErrorTheSameId = errors.New("cannot use the same uuid for 'id' and 'parent_id' fields")
 var ErrorProjectId = errors.New("not valid 'project_id'")
 
-type StorageI interface {
+type StorageI interface { 
 	CloseDB()
 	Project() ProjectRepoI
 }
