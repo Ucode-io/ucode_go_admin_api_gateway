8fd57e94af65b93caa0574f9aa4421036a6d81bd
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2024-10-07 15:16:26 +0500

    get creds for go object builder from config

--- services/go_builder_service.go
+++ services/go_builder_service.go
@@ -58,9 +58,9 @@ func NewGoBuilderServiceClient(ctx context.Context, cfg config.Config) (GoBuilde
 
 	connGoBuilderService, err := grpc.DialContext(
 		ctx,
-		// cfg.GoObjectBuilderServiceHost+cfg.GoObjectBuilderGRPCPort,
+		cfg.GoObjectBuilderServiceHost+cfg.GoObjectBuilderGRPCPort,
 		// "go-object-builder-service:80",
-		"localhost:7107",
+		// "localhost:7107",
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 
