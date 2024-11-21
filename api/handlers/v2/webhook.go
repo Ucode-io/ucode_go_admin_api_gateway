4011cf0612335b9f317e000c270279be2c05d026
Abdulbosit <abdulbositkabilov1@gmail.com>
2024-11-21 15:51:16 +0500

    edit param to query

--- api/handlers/v2/webhook.go
+++ api/handlers/v2/webhook.go
@@ -17,6 +17,7 @@ import (
 	"ucode/ucode_go_api_gateway/genproto/new_function_service"
 	"ucode/ucode_go_api_gateway/pkg/github"
 	"ucode/ucode_go_api_gateway/pkg/gitlab"
+	"ucode/ucode_go_api_gateway/pkg/logger"
 	"ucode/ucode_go_api_gateway/services"
 
 	"github.com/gin-gonic/gin"
@@ -25,11 +26,13 @@ import (
 )
 
 func (h *HandlerV2) GithubLogin(c *gin.Context) {
-	fmt.Println("HERER REQUESTED")
+	fmt.Println("HERE REQUESTED")
 	var (
-		code                  = c.Param("code")
+		code                  = c.Query("code")
 		accessTokenUrl string = "https://github.com/login/oauth/access_token"
 	)
+
+	h.log.Info("CODE", logger.Any("code", code))
 
 	param := map[string]interface{}{
 		"client_id":     h.baseConf.GithubClientId,
