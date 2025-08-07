9c7ec87297d5d4aa28307ed0a6f757221c2b3a5d
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2025-08-07 10:20:41 +0500

    update proto file names

--- storage/storage.go
+++ storage/storage.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"time"
-	pb "ucode/ucode_go_api_gateway/genproto/project_service"
 )
 
 var ErrorTheSameId = errors.New("cannot use the same uuid for 'id' and 'parent_id' fields")
@@ -12,15 +11,6 @@ var ErrorProjectId = errors.New("not valid 'project_id'")
 
 type StorageI interface {
 	CloseDB()
-	Project() ProjectRepoI
-}
-
-type ProjectRepoI interface {
-	Create(ctx context.Context, entity *pb.CreateProjectRequest) (pKey *pb.ProjectPrimaryKey, err error)
-	GetList(ctx context.Context, queryParam *pb.GetAllProjectsRequest) (res *pb.GetAllProjectsResponse, err error)
-	GetByPK(ctx context.Context, pKey *pb.ProjectPrimaryKey) (res *pb.Project, err error)
-	Update(ctx context.Context, entity *pb.UpdateProjectRequest) (rowsAffected int64, err error)
-	Delete(ctx context.Context, pKey *pb.ProjectPrimaryKey) (namespace string, rowsAffected int64, err error)
 }
 
 type RedisStorageI interface {
