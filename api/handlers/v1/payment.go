b9292a571edf81e11913b938c8a1069450fab303
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2025-10-03 18:36:20 +0500

    add stripe migrations

--- api/handlers/v1/payment.go
+++ api/handlers/v1/payment.go
@@ -1,16 +1,24 @@
 package v1
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stripe/stripe-go/v83"
 	"github.com/stripe/stripe-go/v83/customer"
-	"github.com/stripe/stripe-go/v83/paymentmethod"
 	"github.com/stripe/stripe-go/v83/setupintent"
+	"github.com/stripe/stripe-go/v83/webhook"
 
 	"ucode/ucode_go_api_gateway/api/status_http"
+	pb "ucode/ucode_go_api_gateway/genproto/company_service"
+	"ucode/ucode_go_api_gateway/pkg/logger"
+	"ucode/ucode_go_api_gateway/pkg/util"
 )
 
 type createPaymentIntentRequest struct {
@@ -23,61 +31,63 @@ type createPaymentIntentRequest struct {
 
 // CreatePaymentIntent calls Stripe to create a real SetupIntent (uses configured Stripe key)
 func (h *HandlerV1) CreatePaymentIntent(c *gin.Context) {
-	var req createPaymentIntentRequest
+	var (
+		req        createPaymentIntentRequest
+		customerId string
+	)
+
 	if err := c.ShouldBindJSON(&req); err != nil {
 		h.handleResponse(c, status_http.BadRequest, "Invalid JSON")
 		return
 	}
 
-	// For SetupIntents, amount/currency are not required; ignoring provided values
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 20*time.Second)
+	defer cancel()
+
+	projectId, ok := c.Get("project_id")
+	if !ok || !util.IsValidUUID(projectId.(string)) {
+		h.handleResponse(c, status_http.InvalidArgument, "project id is an invalid uuid")
+		return
+	}
 
 	stripe.Key = "sk_test_51QvC6qCx1p2EqOQp37eMRD73jmsECnITZ1eYTn4BbYv8uLNUfGOJUf3X0j14fyjhAvcoZYucz9oCy1aEJrg7Yyp300ScU9kgfh"
 
-	params := &stripe.CustomerParams{}
-	if req.Email != "" {
-		params.Email = stripe.String(req.Email)
-	}
-	if req.Phone != "" {
-		params.Phone = stripe.String(req.Phone)
-	}
-
-	cus, err := customer.New(params)
+	project, err := h.companyServices.Project().GetById(ctx, &pb.GetProjectByIdRequest{ProjectId: projectId.(string)})
 	if err != nil {
 		h.handleResponse(c, status_http.InternalServerError, err.Error())
 		return
 	}
 
-	pm := &stripe.PaymentMethodParams{
-		// Customer: stripe.String(cus.ID),
-		Card: &stripe.PaymentMethodCardParams{
-			Token: stripe.String("tok_visa"), // Using a test token; in real scenarios, use Stripe.js to create tokens securely
-		},
-		Type: stripe.String("card"),
-	}
+	if project.CustomerId == "" {
+		params := &stripe.CustomerParams{}
+		if req.Email != "" {
+			params.Email = stripe.String(req.Email)
+		}
+		if req.Phone != "" {
+			params.Phone = stripe.String(req.Phone)
+		}
 
-	paymentMethod, err := paymentmethod.New(pm)
-	if err != nil {
-		fmt.Println("paymentMethod err", err)
-		h.handleResponse(c, status_http.InternalServerError, err.Error())
-		return
-	}
+		customer, err := customer.New(params)
+		if err != nil {
+			h.handleResponse(c, status_http.InternalServerError, err.Error())
+			return
+		}
 
-	_, err = paymentmethod.Attach(paymentMethod.ID, &stripe.PaymentMethodAttachParams{
-		Customer: stripe.String(cus.ID),
-	})
-	if err != nil {
-		fmt.Println("attach err", err)
-		h.handleResponse(c, status_http.InternalServerError, err.Error())
-		return
-	}
+		customerId = customer.ID
 
-	// card.List(&stripe.CardListParams{
-	// 	Customer: stripe.String(cus.ID),
-	// })
+		_, err = h.companyServices.Project().AttachCustomer(ctx, &pb.AttachCustomerRequest{
+			ProjectId:  projectId.(string),
+			CustomerId: customerId,
+		})
+		if err != nil {
+			h.handleResponse(c, status_http.InternalServerError, err.Error())
+			return
+		}
+	}
 
 	intentParams := &stripe.SetupIntentParams{
-		Customer:      stripe.String(cus.ID),
-		PaymentMethod: &paymentMethod.ID,
+		Customer:           stripe.String(customerId),
+		PaymentMethodTypes: []*string{stripe.String("card")},
 	}
 
 	setI, err := setupintent.New(intentParams)
@@ -87,73 +97,68 @@ func (h *HandlerV1) CreatePaymentIntent(c *gin.Context) {
 		return
 	}
 
-	// paymentIntentParams := &stripe.PaymentIntentParams{
-	// 	Amount:        stripe.Int64(req.Amount),
-	// 	Currency:      stripe.String(string(stripe.CurrencyUSD)),
-	// 	Customer:      stripe.String(cus.ID),
-	// 	PaymentMethod: &paymentMethod.ID,
-	// 	Confirm:       stripe.Bool(true),
-	// 	OffSession:    stripe.Bool(true),
-	// 	// ReceiptEmail:  stripe.String(req.Email),
-	// }
-	// paymentItent, err := paymentintent.New(paymentIntentParams)
-	// if err != nil {
-	// 	fmt.Println("paymentintent err", err)
-	// 	h.handleResponse(c, status_http.InternalServerError, err.Error())
-	// 	return
-	// }
-
-	// return the Stripe SetupIntent directly for maximum fidelity
 	h.handleResponse(c, status_http.Created, setI)
 }
 
 func (h *HandlerV1) StripeWebhook(c *gin.Context) {
-	// var payload []byte
-	var payload = make(map[string]any)
+	var payload []byte
+	// var payload = make(map[string]any)
 	if err := c.ShouldBindJSON(&payload); err != nil {
 		h.handleResponse(c, status_http.BadRequest, "Invalid JSON")
 		return
 	}
 
-	asd, _ := json.Marshal(payload)
-	fmt.Println("payload", string(asd))
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 20*time.Second)
+	defer cancel()
 
-	// endpointSecret := "whsec_cOGBaP6EVo4kRUCfeKXuSWg0JAL2avRg"
-	// signatureHeader := c.GetHeader("Stripe-Signature")
-	// event, err := webhook.ConstructEvent(payload, signatureHeader, endpointSecret)
-	// if err != nil {
-	// 	h.handleResponse(c, status_http.InternalServerError, "Webhook signature verification failed."+err.Error())
-	// 	return
-	// }
+	fmt.Println("payload", string(payload))
 
-	// fmt.Println("Received event: ", string(event.Data.Raw))
+	endpointSecret := "whsec_cOGBaP6EVo4kRUCfeKXuSWg0JAL2avRg"
+	signatureHeader := c.GetHeader("Stripe-Signature")
+	event, err := webhook.ConstructEvent(payload, signatureHeader, endpointSecret)
+	if err != nil {
+		h.handleResponse(c, status_http.InternalServerError, "Webhook signature verification failed."+err.Error())
+		return
+	}
 
-	// switch event.Type {
-	// case "payment_intent.succeeded":
-	// 	var paymentIntent stripe.PaymentIntent
-	// 	err := json.Unmarshal(event.Data.Raw, &paymentIntent)
-	// 	if err != nil {
-	// 		h.log.Error("Error parsing webhook JSON: ", logger.Error(err))
-	// 		h.handleResponse(c, status_http.InternalServerError, err.Error())
-	// 		return
-	// 	}
+	fmt.Println("Received event: ", string(event.Data.Raw))
 
-	// 	log.Printf("Successful payment for %d.", paymentIntent.Amount)
-	// 	// Then define and call a func to handle the successful payment intent.
-	// 	// handlePaymentIntentSucceeded(paymentIntent)
-	// case "payment_method.attached":
-	// 	var paymentMethod stripe.PaymentMethod
-	// 	err := json.Unmarshal(event.Data.Raw, &paymentMethod)
-	// 	if err != nil {
-	// 		h.log.Error("Error parsing webhook JSON: ", logger.Error(err))
-	// 		h.handleResponse(c, status_http.InternalServerError, err.Error())
-	// 		return
-	// 	}
-	// 	// Then define and call a func to handle the successful attachment of a PaymentMethod.
-	// 	// handlePaymentMethodAttached(paymentMethod)
-	// default:
-	// 	fmt.Fprintf(os.Stderr, "Unhandled event type: %s\n", event.Type)
-	// }
+	switch event.Type {
+	case "payment_intent.succeeded":
+		var paymentIntent stripe.PaymentIntent
+		err := json.Unmarshal(event.Data.Raw, &paymentIntent)
+		if err != nil {
+			h.log.Error("Error parsing webhook JSON: ", logger.Error(err))
+			h.handleResponse(c, status_http.InternalServerError, err.Error())
+			return
+		}
+
+		log.Printf("Successful payment for %d.", paymentIntent.Amount)
+		// Then define and call a func to handle the successful payment intent.
+		// handlePaymentIntentSucceeded(paymentIntent)
+	case "payment_method.attached":
+		var paymentMethod stripe.PaymentMethod
+		err := json.Unmarshal(event.Data.Raw, &paymentMethod)
+		if err != nil {
+			h.log.Error("Error parsing webhook JSON: ", logger.Error(err))
+			h.handleResponse(c, status_http.InternalServerError, err.Error())
+			return
+		}
+		// Then define and call a func to handle the successful attachment of a PaymentMethod.
+		h.handlePaymentMethodAttached(ctx, paymentMethod)
+	default:
+		fmt.Fprintf(os.Stderr, "Unhandled event type: %s\n", event.Type)
+	}
 
 	h.handleResponse(c, status_http.Created, status_http.OK)
+}
+
+func (h *HandlerV1) handlePaymentMethodAttached(ctx context.Context, paymentMethod stripe.PaymentMethod) {
+	h.companyServices.Billing().CreateCard(ctx, &pb.CreateProjectCardRequest{
+		Pan:        paymentMethod.Card.Last4,
+		Expire:     fmt.Sprintf("%d/%d", paymentMethod.Card.ExpMonth, paymentMethod.Card.ExpYear),
+		ProjectId:  paymentMethod.Customer.ID,
+		Type:       strings.ToUpper(string(paymentMethod.Card.Brand)),
+		ExternalId: paymentMethod.ID,
+	})
 }
