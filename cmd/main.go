ee1c4f4019b11a41d7e891ac791c8aeb1d9214e0
Shokhrukh <shokhrukhsafarov5@gmail.com>
2026-04-03 18:02:59 +0500

    removed logs

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"ucode/ucode_go_api_gateway/api"
 	"ucode/ucode_go_api_gateway/api/handlers"
 	"ucode/ucode_go_api_gateway/config"
@@ -125,11 +124,6 @@ func main() {
 	r.Use(gin.Logger(), gin.Recovery())
 
 	limiter := util.NewApiKeyRateLimiter(newRedis, config.RATE_LIMITER_RPS_LIMIT, config.RATE_LIMITER_RPS_LIMIT)
-
-	fmt.Println("Address->", baseConf.VaultAddress)
-	fmt.Println("role id->", baseConf.VaultRoleID)
-	fmt.Println("secret id->", baseConf.VaultSecretID)
-	fmt.Println("mount path->", baseConf.VaultMountPath)
 
 	var vaultClient vault.VaultClient
 	if baseConf.VaultAddress != "" {
