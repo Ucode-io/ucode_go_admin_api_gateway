6aa476d559dab590a73c929a7930ef35b325acd3
mac <hayrullaev008@gmail.com>
2026-04-09 15:10:02 +0500

    api/api.go edited

--- api/api.go
+++ api/api.go
@@ -63,8 +63,8 @@ func SetUpAPI(r *gin.Engine, h handlers.Handler, cfg config.BaseConfig, tracer o
 
 		// Pricing
 		v1.GET("/pricing/all", h.V1.GetAllPricingUsage)
-		v1.GET("/pricing/api-metrics", h.V1.GetApiMetrics)
-		v1.GET("/pricing/api-chart", h.V1.GetApiChart)
+		v1.GET("/pricing/api-call/api-metrics", h.V1.GetApiMetrics)
+		v1.GET("/pricing/api-call/api-chart", h.V1.GetApiChart)
 
 		// MINIO
 		v1.POST("/minio/bucket-size", h.V1.BucketSize)
